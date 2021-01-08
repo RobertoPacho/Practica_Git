5343e274a0860a11038ffd8ce823b431432d2253
“[narcisaaraujo]” <[araujoperesnarcisadejesus@gmail.com]>
2021-01-08 01:10:47 -0500

    inicio

---
+++ src/ec/ups/edu/git/Logica.java
@@ -0,0 +1,47 @@
+package ec.ups.edu.git;
+
+public class Logica {
+	private int id;
+	private String nombre;
+	private String apellido;
+	
+	
+	public Logica() {
+
+	}
+	
+	public Logica(int id, String nombre, String apellido) {
+		this.id = id;
+		this.nombre = nombre;
+		this.apellido = apellido;
+	}
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getApellido() {
+		return apellido;
+	}
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+	
+	public String metodIntegranteUno() {
+		String mensaje="Narcisa Araujo";
+		return mensaje;
+	}
+	public String metodIntegranteDos() {
+		String mensaje= "Roberto Pacho";
+		return mensaje;
+	}
+
+}
---
+++ src/ec/ups/edu/git/Principal.java
@@ -0,0 +1,17 @@
+package ec.ups.edu.git;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Logica logica=new Logica();
+                
+		System.out.println(logica.metodIntegranteUno());
+                
+                System.out.println(logica.metodIntegranteDos());
+				
+	
+
+	}
+
+}
